4aac48e481dd13e40f41277c0c912132c4f166f8
Christian <69398172+soy-chrislo@users.noreply.github.com>
2023-05-06 14:19:27 -0500

    Update setup.sh

--- setup.sh/setup.sh
+++ setup.sh/setup.sh
@@ -7,6 +7,7 @@ sudo apt install -y neovim git neofetch curl wget htop &&
 ## Install ZSH
 sudo apt install -y zsh &&
 sudo chsh -s $(which zsh) &&
+echo "exec zsh" >> ~/.bashrc
 
 # Install Visual Studio Code
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg &&
@@ -18,7 +19,7 @@ sudo apt-get install code &&
 ## Install Node using NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash &&
 ### Bash
-source ~/.bashrc &&
+# source ~/.bashrc &&
 ### ZSH
 source ~/.zshrc &&
 nvm install node &&
@@ -34,20 +35,19 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc &&
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc &&
 echo 'eval "$(pyenv init -)"' >> ~/.zshrc &&
 exec "$SHELL" &&
-pyenv install &&
+pyenv install 3.11.3 &&
 
 
 ## Install Docker
 sudo apt -y update &&
 sudo apt -y install apt-transport-https ca-certificates curl software-properties-common &&
-sudo apt -y remove docker docker-engine docker.io containerd runc &&
+# sudo apt -y remove docker docker-engine docker.io containerd runc &&
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg &&
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu jammy stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
 
 sudo apt -y update &&
-sudo apt update &&
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin &&
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin &&
 
 sudo groupadd docker &&
 sudo usermod -aG docker $USER &&
@@ -58,44 +58,45 @@ sudo systemctl enable containerd.service &&
 sudo systemctl start docker.service &&
 sudo systemctl start containerd.service &&
 
-## PNPM
+## PNPM (problema al reload)
 wget -qO- https://get.pnpm.io/install.sh | sh - &&
 ### Bash
-source ~/.bashrc &&
+# source ~/.bashrc &&
 ### ZSH
 source ~/.zshrc &&
 corepack enable &&
 
-## Phpbrew
-sudo apt-get install \
-  build-essential \
-  libbz2-dev \
-  libreadline-dev \
-  libsqlite3-dev \
-  libcurl4-gnutls-dev \
-  libzip-dev \
-  libssl-dev \
-  libxml2-dev \
-  libxslt-dev \
-  php8.1-cli \
-  php8.1-bz2 \
-  php8.1-xml \
-  pkg-config \
-  libonig-dev -y &&
+## Phpbrew (instalado pero error al instalar php)
+#sudo apt-get install \
+#  build-essential \
+#  libbz2-dev \
+#  libreadline-dev \
+#  libsqlite3-dev \
+#  libcurl4-gnutls-dev \
+#  libzip-dev \
+#  libssl-dev \
+#  libxml2-dev \
+#  libxslt-dev \
+#  php8.1-cli \
+#  php8.1-bz2 \
+#  php8.1-xml \
+#  pkg-config \
+#  libonig-dev -y &&
 
-curl -L -O https://github.com/phpbrew/phpbrew/releases/latest/download/phpbrew.phar &&
-chmod +x phpbrew.phar &&
-sudo mv phpbrew.phar /usr/local/bin/phpbrew &&
+#curl -L -O https://github.com/phpbrew/phpbrew/releases/latest/download/phpbrew.phar &&
+#chmod +x phpbrew.phar &&
+#sudo mv phpbrew.phar /usr/local/bin/phpbrew &&
 
-phpbrew init &&
+#phpbrew init &&
 
-echo '[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc' >> ~/.zshrc &&
-source ~/.zshrc &&
-phpbew update &&
-phpbrew install php-8.2 +default &&
+#echo '[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc' >> ~/.zshrc &&
+#source ~/.zshrc &&
+#phpbrew update &&
+#phpbrew install php-8.2 +default &&
 
 ## Java JDK
-sudo apt-get update && apt-get remove openjdk* &&
+sudo apt-get update && 
+# apt-get remove openjdk* &&
 sudo apt-get install openjdk-8-jdk -y &&
 sudo apt-get install openjdk-17-jdk -y &&
 
@@ -111,7 +112,7 @@ sudo add-apt-repository ppa:longsleep/golang-backports &&
 sudo apt update &&
 sudo apt install golang-go -y &&
 
-## Install Deno using DVM (Not working)
+## Install Deno using DVM (Work)
 curl -fsSL https://dvm.deno.dev | sh &&
 ### Bash
 source ~/.bashrc && source ~/.bash_profile &&
@@ -135,6 +136,7 @@ sudo apt update &&
 sudo apt install obs-studio -y &&
 
 ## Install Discord
+sudo apt install libc++1-14 gconf2-common libgconf-2-4 libc++1 && apt --fix-broken install -y &&
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" &&
 # wget -O discord “https://discord.com/api/download?platform=linux&format=deb”
 sudo dpkg -i discord.deb &&
@@ -151,3 +153,73 @@ sudo apt-get upgrade -y &&
 sudo apt-get install gimp -y &&
 
 ## Android SDK 
+
+## MultiMC
+cd &&
+wget https://files.multimc.org/downloads/multimc_1.6-1.deb &&
+sudo apt install -y ./multimc_1.6-1.deb
+
+## Insomnia
+cd &&
+wget https://github.com/Kong/insomnia/releases/download/core@2023.2.0/Insomnia.Core-2023.2.0.deb &&
+sudo apt install -y ./Insomnia.Core-2023.2.0.deb &&
+
+## Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt-get -y update && sudo apt-get install -y yarn && yarn
+
+## Battery Treshold Asus
+# cd /etc/systemd/system &&
+# sudo touch battery-charge-threshold.service &&
+
+# sudo echo "[Unit]" >> battery-charge-threshold.service &&
+# sudo echo "Description=Set the battery charge threshold" >> battery-charge-threshold.service &&
+# sudo echo "After=multi-user.target" >> battery-charge-threshold.service &&
+# sudo echo "StartLimitBurst=0" >> battery-charge-threshold.service &&
+# sudo echo " " >> battery-charge-threshold.service &&
+# sudo echo "[Service]" >> battery-charge-threshold.service &&
+# sudo echo "Type=oneshot" >> battery-charge-threshold.service &&
+# sudo echo "Restart=on-failure" >> battery-charge-threshold.service &&
+# sudo echo "ExecStart=/bin/bash -c 'echo 60 > /sys/class/power_supply/BAT0/charge_control_end_threshold'" >> battery-charge-threshold.service &&
+# sudo echo " " >> battery-charge-threshold.service &&
+# sudo echo "[Install]" >> battery-charge-threshold.service &&
+# sudo echo "WantedBy=multi-user.target" >> battery-charge-threshold.service &&
+
+# sudo systemctl enable battery-charge-threshold.service &&
+# sudo systemctl start battery-charge-threshold.service &&
+
+## Dart
+
+sudo apt-get update &&
+
+sudo apt-get install apt-transport-https &&
+
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg &&
+
+echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list &&
+
+sudo apt-get update &&
+sudo apt-get install dart &&
+
+
+## Google Chrome Stable
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
+sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main" &&
+sudo apt update &&
+sudo apt install -y google-chrome-stable &&
+
+## Intellij Idea Ultimate
+cd &&
+wget https://download.jetbrains.com/idea/ideaIU-2023.1.1.tar.gz &&
+sudo tar -xzf ideaIU-2023.1.1.tar.gz -C /opt &&
+cd /opt/idea-IU-231.8770.65 &&
+bash ./bin/idea.sh &&
+
+
+## VmWare
+sudo apt update &&
+sudo apt install build-essential gcc wget linux-headers-generic linux-headers-$(uname -r) &&
+cd /tmp &&
+wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0" https://www.vmware.com/go/getWorkstation-linux &&
+chmod +x getWorkstation-linux &&
+sudo ./getWorkstation-linux &&
+
